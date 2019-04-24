3cafaf92d285ee3ad2724120da10106630ec9575
Antonio Cuadrado <cuatova7@gmail.com>
2019-04-24 12:20:59 -0300

    ejercicio

--- Class6.sql
+++ Class6.sql
@@ -1,5 +1,3 @@
-Antonio
-
 SELECT last_name from actor a1 WHERE EXISTS(SELECT * from actor a2 WHERE a1.last_name = a2.last_name) order by last_name ASC
 
 SELECT a.first_name FROM actor a, film_actor fa WHERE a.actor_id = fa.actor_id GROUP BY a.actor_id
@@ -10,6 +8,14 @@ SELECT c.first_name FROM customer c, rental r WHERE c.customer_id = r.customer_i
 
 SELECT first_name, a.actor_id FROM actor a WHERE a.actor_id IN (SELECT f.actor_id FROM film_actor f WHERE f.film_id IN 
 (SELECT fm.film_id FROM film fm WHERE fm.title = 'BETRAYED REAR' OR fm.title = 'CATCH AMISTAD')) 
+
+SELECT * FROM actor a WHERE a.actor_id IN (SELECT f.actor_id FROM film_actor f WHERE f.film_id IN 
+(SELECT fm.film_id FROM film fm WHERE fm.title = 'BETRAYED REAR') AND a.actor_id IN (SELECT f.actor_id FROM film_actor f WHERE f.film_id IN (
+SELECT fm.film_id FROM film fm WHERE fm.title = 'CATCH AMISTAD')))
+
+SELECT * FROM actor a WHERE a.actor_id NOT IN (SELECT f.actor_id FROM film_actor f WHERE f.film_id IN 
+(SELECT fm.film_id FROM film fm WHERE fm.title = 'BETRAYED REAR')) AND a.actor_id NOT IN (SELECT f.actor_id 
+FROM film_actor f WHERE f.film_id IN (SELECT fm.film_id FROM film fm WHERE fm.title = 'CATCH AMISTAD'));
 
 
 
